b11ade933610280f0922899eec7530cf4245ca4a
33aodan <dan.wnrs@gmail.com>
2023-08-11 19:06:22 -0500

    hi

---
+++ main.py
@@ -0,0 +1,10 @@
+def area(width,length):
+    area=width*length
+    return area
+
+width = int(input("input number for width: "))
+length = int(input("input number for length: "));
+
+result=area(width,length)
+
+print("the area of a rectangle is: ",result," units squared")
